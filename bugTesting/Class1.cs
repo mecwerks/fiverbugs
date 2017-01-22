c523bd2163eef025dd32bbdb3baa653aa3a7c37a
Marcus Collins <mecwerks@gmail.com>
2017-01-22 14:17:18 -0500

    Initial Commit

--- bugTesting/Class1.cs
+++ bugTesting/Class1.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CitizenFX.Core;
 
 namespace bugTesting
 {
-    public class Class1
+    //
+    // failed to load type VehicleModCollection
+    public class Bug_VehicleModCollection : BaseScript
     {
+        public Bug_VehicleModCollection()
+        {
+            Debug.Write("Testing type " + typeof(VehicleModCollection));
+            Debug.WriteLine();
+            Debug.Write("Testing type " + typeof(VehicleMod));
+            Debug.WriteLine();
+        }
+    }
+
+    //
+    // Weapon.Ammo returns 0 on first call
+    // Weapons.HasWeapon() returns false on first call
+    public class Bug_AmmoHasWeapFalse : BaseScript
+    {
+        public Bug_AmmoHasWeapFalse()
+        {
+            Tick += async () =>
+            {
+                if (Game.IsControlPressed(Game.CurrentInputMode == InputMode.GamePad ? 0 : 1, Control.MultiplayerInfo))
+                {
+                    if (LocalPlayer.Character.Weapons.Current != WeaponHash.Unarmed)
+                    {
+                        Debug.Write("Current Weapon: " + LocalPlayer.Character.Weapons.Current.Hash + " Ammo: " + LocalPlayer.Character.Weapons.Current.Ammo);
+                        Debug.WriteLine();
+
+                        Debug.Write("Has Current Weapon: " + LocalPlayer.Character.Weapons.HasWeapon(LocalPlayer.Character.Weapons.Current));
+                        Debug.WriteLine();
+                    }
+                }
+
+                await Task.FromResult(0);
+            };
+        }
     }
 }
